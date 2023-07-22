df6b15d5727e4f0f36dcf027f5fb01e87af37dde
mazharaa <mazharaa32@gmail.com>
2023-07-22 19:38:36 +0700

    Add Login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -3,10 +3,46 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  static const String id = 'login_screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Sign in to find your favorite food\'s recipe'),
+          const TextField(
+            decoration: InputDecoration(
+              hintText: 'E-mail'
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+                hintText: 'Password'
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
 
+            },
+            child: const Text('Login')
+          ),
+          ElevatedButton(
+              onPressed: () {
+
+              },
+              child: const Text('Create account')
+          ),
+          TextButton(
+            onPressed: () {
+              
+            }, 
+            child: const Text('Forgot password?')
+          )
+        ],
+      ),
     );
   }
 }
